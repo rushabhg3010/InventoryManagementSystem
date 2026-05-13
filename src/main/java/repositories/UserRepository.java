23e4b4cf63ca58f39bb631f295969ed782e0e0d5
rushabhg3010 <rushabhg3010@gmail.com>
2026-05-13 23:03:32 +0530

    created a user repository

---
+++ src/main/java/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	Optional<User> findByEmail(String email);
+
+}
