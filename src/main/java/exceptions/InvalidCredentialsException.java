b7acc95c1a8e27a1e03a4be95c725340e20ebedb
rushabhg3010 <rushabhg3010@gmail.com>
2026-05-16 15:52:24 +0530

    created invalid credentials exception class

---
+++ src/main/java/exceptions/InvalidCredentialsException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class InvalidCredentialsException extends RuntimeException {
+	
+	public InvalidCredentialsException(String message) {
+        super(message);
+    }
+
+}
