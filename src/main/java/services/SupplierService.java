8efbbdf8466f6612264c95c8fab4bcde8ce06f50
rushabhg3010 <rushabhg3010@gmail.com>
2026-05-18 14:55:43 +0530

    created a supplier service

---
+++ src/main/java/services/SupplierService.java
@@ -0,0 +1,18 @@
+package services;
+
+import dtos.Response;
+import dtos.SupplierDTO;
+
+public interface SupplierService {
+	
+	Response addSupplier(SupplierDTO supplierDTO);
+
+    Response updateSupplier(Long id, SupplierDTO supplierDTO);
+
+    Response getAllSupplier();
+
+    Response getSupplierById(Long id);
+
+    Response deleteSupplier(Long id);
+
+}
